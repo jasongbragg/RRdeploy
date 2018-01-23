002af38bbba8e96e83988f307f0ebcc49289438c
jasongbragg <jasongbragg@gmail.com>
2018-01-23 11:24:29 +1100

    compatibility with server wrapper

--- R/generate_GDM_prediction.r
+++ R/generate_GDM_prediction.r
@@ -1,27 +1,62 @@
 #' Generates a prediction for a genetically local area
 #' for a selected species and location
 #' 
-#' @param  selected_location -- a selected point [Required]
-#' @param  gdm_model_file    -- an R object bundling everything needed for model [Required]
-
+#' @param  longitude         -- selected longitude
+#' @param  latitude          -- selected latitude
+#' @param  gdm_model         -- an R list bundling everything required to predict under a GDM 
+#' @param  gdm_model_file    -- an R object file containing a model list (called "md")
+#' @param  species_name      -- name of species (as it appears in a gdm model file, which ends "_genetic_model.Rd"
+#' @param  model_dir         -- directory in which gdm model can be found 
+#' @param  out_dir           -- location to place raster of results (if NULL, return raster object)
 #' @return a raster containing the prediction of genetically local area
 #' @author Jason Bragg (jasongbragg@gmail.com)
 #' @export
 #' @examples
 #' \dontrun{
-#' generate_GDM_prediction <- function( c(150.45, -33.66), CallSerr_genetic_model.Rd )
+#' generate_GDM_prediction <- function( longitude=150.45, latitude=-33.66, gdm_model_file="CallSerr_genetic_model.Rd", out_dir )
 #' }
 
-generate_GDM_prediction <- function(selected_location, gdm_model_file) {
+generate_GDM_prediction <- function(longitude, latitude, gdm_model_file=NULL, species_name=NULL, model_dir=NULL, gdm_model=NULL, out_dir=NULL) {
 
-   s1_point <- selected_location
+   s1_point <- c(longitude, latitude) 
    
    ### load required packages
    require(gdm); require(raster); require(fields); require(sp);
 
-   ### load object containing model
-   ### and all data required to run it
-   load(gdm_model_file)
+   md <- NULL
+
+   ### if model is supplied, use it
+   if ( !is.null(gdm_model) ) {
+      md <- gdm_model
+   } else {
+
+      # if model is not supplied as list
+      # first see if file is supplied
+      if (!is.null(gdm_model_file)) {
+
+         gmf <- gdm_model_file
+
+      } else {
+
+         # if file name not supplied
+         # attempt to deduce file name with species name and directory
+         # if still not found, error
+         if ( is.null(species_name) | is.null(model_dir) ) {
+            cat("  No model provided. Must specify either:                       \n")
+            cat("                             model list object                  \n")
+            cat("                             a file containing object           \n")
+            cat("                             or taxon name plus model directory \n")
+            stop()
+         }
+         gfm <- paste(model_dir, species_name, "_genetic_model.Rd", sep="")
+
+      }
+      load(gmf)
+   }
+
+   if (is.null(md)) {
+      cat( " Something went wrong. GDM model object not found by generate_GDM_prediction() function \n" )
+   }
 
    sdata <- md$sdata
    r.pts <- rasterToPoints(sdata, spatial=TRUE)
@@ -79,7 +114,16 @@ generate_GDM_prediction <- function(selected_location, gdm_model_file) {
    thresh <- md$threshold
    genetically_local_raster <-  clip_prediction
    values(genetically_local_raster)[values(genetically_local_raster) > thresh ] <- NA
+   values(genetically_local_raster)[ !is.na(values(genetically_local_raster)) ] <- 1
 
-   return(genetically_local_raster)
+   if ( is.null(out_dir) ) {
+      return(genetically_local_raster)
+   } else {
+      # get taxon name from the model file name
+      tmpGenLocalRasterFile <- paste(sessionDir,"/geneticLocal.tif",sep="")
+      writeRaster(genetically_local_raster, tmpGenLocalRasterFile, format="GTiff", overwrite = TRUE)
+      return(tmpGenLocalRasterFile)
+   }
+   
 }
 
